284ce91de24a44bdb5752387ad2ddbbe040a1522
range3 <white.line.oar@gmail.com>
2021-12-27 08:03:50 +0000

    Impl ADIOI_PMEM_ReadContig | Flush

--- src/mpi/romio/adio/ad_pmem/ad_pmem_flush.c
+++ src/mpi/romio/adio/ad_pmem/ad_pmem_flush.c
@@ -4,15 +4,44 @@
  */
 
 #include "ad_pmem.h"
+#include "ad_pmem_common.h"
 #include "adioi.h"
+#include "mpi.h"
+#include "pmembb/pmembb.h"
+#include <fcntl.h>
 
-void ADIOI_PMEM_Flush(ADIO_File fd, int *error_code)
-{
-    int myrank, nprocs;
+static bool flush_pmembb_cb(const char *data, size_t size, off64_t ofs,
+                            void *userdata, int *error_code) {
+#ifdef DEBUG
+  FPRINTF(stdout, "    !!!flushing (buf = %p, loc = %lld, sz = %lld)\n", data,
+          (long long)ofs, (long long)size);
+#endif
+  ADIOI_GEN_WriteContig(userdata, data, size, MPI_BYTE, ADIO_EXPLICIT_OFFSET,
+                        ofs, NULL, error_code);
+  return *error_code == MPI_SUCCESS;
+}
 
-    *error_code = MPI_SUCCESS;
+void ADIOI_PMEM_Flush(ADIO_File fd, int *error_code) {
+#if defined(MPICH) || !defined(PRINT_ERR_MSG)
+  static char myname[] = "ADIOI_PMEM_Flush";
+#endif
+  int myrank, nprocs;
+  int flush_error_code;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_Flush called on %s\n", myrank, nprocs, fd->filename);
+  *error_code = MPI_SUCCESS;
+
+#ifdef DEBUG
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_Flush called on %s\n", myrank, nprocs,
+          fd->filename);
+#endif
+
+  ADIOI_PMEM_fs *pmemfs = fd->fs_ptr;
+  flush_error_code = pmembb_bb_flush_all(pmemfs->bb, fd, flush_pmembb_cb);
+  if (flush_error_code) {
+    *error_code =
+        ADIOI_Err_create_code(myname, fd->filename, -flush_error_code);
+    return;
+  }
 }
--- src/mpi/romio/adio/ad_pmem/ad_pmem_read.c
+++ src/mpi/romio/adio/ad_pmem/ad_pmem_read.c
@@ -7,49 +7,64 @@
 #include "adioi.h"
 
 void ADIOI_PMEM_ReadContig(ADIO_File fd, void *buf, int count,
-                             MPI_Datatype datatype, int file_ptr_type,
-                             ADIO_Offset offset, ADIO_Status * status, int
-                             *error_code)
-{
-    int myrank, nprocs;
-    MPI_Count datatype_size;
+                           MPI_Datatype datatype, int file_ptr_type,
+                           ADIO_Offset offset, ADIO_Status *status,
+                           int *error_code) {
+  int myrank, nprocs;
+  MPI_Count datatype_size, len;
 
-    *error_code = MPI_SUCCESS;
+  *error_code = MPI_SUCCESS;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    MPI_Type_size_x(datatype, &datatype_size);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_ReadContig called on %s\n", myrank, nprocs, fd->filename);
-    if (file_ptr_type != ADIO_EXPLICIT_OFFSET) {
-        offset = fd->fp_ind;
-        fd->fp_ind += datatype_size * count;
-        fd->fp_sys_posn = fd->fp_ind;
-    } else {
-        fd->fp_sys_posn = offset + datatype_size * count;
-    }
+  MPI_Type_size_x(datatype, &datatype_size);
+  len = datatype_size * (ADIO_Offset)count;
+  if (file_ptr_type == ADIO_INDIVIDUAL) {
+    offset = fd->fp_ind;
+  }
 
-    FPRINTF(stdout, "[%d/%d]    reading (buf = %p, loc = %lld, sz = %lld)\n",
-            myrank, nprocs, buf, (long long) offset, (long long) datatype_size * count);
+#ifdef DEBUG
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_ReadContig called on %s\n", myrank,
+          nprocs, fd->filename);
+  FPRINTF(stdout, "[%d/%d]    reading (buf = %p, loc = %lld, sz = %lld)\n",
+          myrank, nprocs, buf, (long long)offset, (long long)len);
+#endif
+
+  ADIOI_PMEM_Flush(fd, error_code);
+  if (*error_code != MPI_SUCCESS) {
+    return;
+  }
+
+  ADIOI_GEN_ReadContig(fd, buf, count, datatype, file_ptr_type, offset, status,
+                       error_code);
+  if (*error_code != MPI_SUCCESS) {
+    return;
+  }
+
+  fd->fp_sys_posn = offset + len;
+  if (file_ptr_type == ADIO_INDIVIDUAL) {
+    fd->fp_ind += len;
+  }
 
 #ifdef HAVE_STATUS_SET_BYTES
-    MPIR_Status_set_bytes(status, datatype, datatype_size * count);
+  MPIR_Status_set_bytes(status, datatype, datatype_size * count);
 #endif
 }
 
 void ADIOI_PMEM_ReadStrided(ADIO_File fd, void *buf, int count,
-                              MPI_Datatype datatype, int file_ptr_type,
-                              ADIO_Offset offset, ADIO_Status * status, int
-                              *error_code)
-{
-    int myrank, nprocs;
+                            MPI_Datatype datatype, int file_ptr_type,
+                            ADIO_Offset offset, ADIO_Status *status,
+                            int *error_code) {
+  int myrank, nprocs;
 
-    *error_code = MPI_SUCCESS;
+  *error_code = MPI_SUCCESS;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_ReadStrided called on %s\n", myrank,
-            nprocs, fd->filename);
-    FPRINTF(stdout, "[%d/%d]    calling ADIOI_GEN_ReadStrided\n", myrank, nprocs);
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_ReadStrided called on %s\n", myrank,
+          nprocs, fd->filename);
+  FPRINTF(stdout, "[%d/%d]    calling ADIOI_GEN_ReadStrided\n", myrank, nprocs);
 
-    ADIOI_GEN_ReadStrided(fd, buf, count, datatype, file_ptr_type, offset, status, error_code);
+  ADIOI_GEN_ReadStrided(fd, buf, count, datatype, file_ptr_type, offset, status,
+                        error_code);
 }
