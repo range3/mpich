667cda25657e887f2f879b28491baa4e1991ce82
range3 <white.line.oar@gmail.com>
2021-12-27 07:39:20 +0000

    Impl ADIOI_PMEM_Fcntl (only supports GET_FSIZE)

--- src/mpi/romio/adio/ad_pmem/ad_pmem_fcntl.c
+++ src/mpi/romio/adio/ad_pmem/ad_pmem_fcntl.c
@@ -4,41 +4,35 @@
  */
 
 #include "ad_pmem.h"
-#include "adioi.h"
 #include "adio_extern.h"
+#include "adioi.h"
 
-void ADIOI_PMEM_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t * fcntl_struct, int *error_code)
-{
-    int myrank, nprocs;
-    static char myname[] = "ADIOI_PMEM_FCNTL";
+void ADIOI_PMEM_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t *fcntl_struct,
+                      int *error_code) {
+  int myrank, nprocs;
+  static char myname[] = "ADIOI_PMEM_FCNTL";
 
-    *error_code = MPI_SUCCESS;
+  *error_code = MPI_SUCCESS;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_Fcntl called on %s\n", myrank, nprocs, fd->filename);
+#ifdef DEBUG
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_Fcntl called on %s\n", myrank, nprocs,
+          fd->filename);
+#endif
 
-    switch (flag) {
-        case ADIO_FCNTL_GET_FSIZE:
-            fcntl_struct->fsize = 0;
-            *error_code = MPI_SUCCESS;
-            break;
+  switch (flag) {
+  case ADIO_FCNTL_GET_FSIZE:
+    ADIOI_GEN_Fcntl(fd, flag, fcntl_struct, error_code);
+    break;
 
-        case ADIO_FCNTL_SET_DISKSPACE:
-            *error_code = MPI_SUCCESS;
-            break;
-
-        case ADIO_FCNTL_SET_ATOMICITY:
-            fd->atomicity = (fcntl_struct->atomicity == 0) ? 0 : 1;
-            *error_code = MPI_SUCCESS;
-            break;
-
-        default:
-            /* --BEGIN ERROR HANDLING-- */
-            *error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-                                               myname, __LINE__,
-                                               MPI_ERR_ARG, "**flag", "**flag %d", flag);
-            return;
-            /* --END ERROR HANDLING-- */
-    }
+  case ADIO_FCNTL_SET_DISKSPACE:
+  case ADIO_FCNTL_SET_ATOMICITY:
+  default:
+    *error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
+                                       myname, __LINE__, MPI_ERR_ARG, "**flag",
+                                       "**flag %d", flag);
+    return;
+    /* --END ERROR HANDLING-- */
+  }
 }
