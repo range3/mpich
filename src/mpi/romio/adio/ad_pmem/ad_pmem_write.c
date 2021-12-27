cd9e2aae4cb0d79031f17bd58ab0fb0fdf75d3fa
range3 <white.line.oar@gmail.com>
2021-12-27 04:30:04 +0000

    Impl ADIOI_PMEM_WriteConting

--- src/mpi/romio/adio/ad_pmem/ad_pmem_write.c
+++ src/mpi/romio/adio/ad_pmem/ad_pmem_write.c
@@ -4,53 +4,72 @@
  */
 
 #include "ad_pmem.h"
+#include "adio.h"
+#include "adio/ad_pmem/ad_pmem_common.h"
 #include "adioi.h"
+#include "pmembb/pmembb.h"
 
 void ADIOI_PMEM_WriteContig(ADIO_File fd, const void *buf, int count,
-                              MPI_Datatype datatype, int file_ptr_type,
-                              ADIO_Offset offset, ADIO_Status * status, int
-                              *error_code)
-{
-    int myrank, nprocs;
-    MPI_Count datatype_size;
+                            MPI_Datatype datatype, int file_ptr_type,
+                            ADIO_Offset offset, ADIO_Status *status,
+                            int *error_code) {
+#if defined(MPICH) || !defined(PRINT_ERR_MSG)
+  static char myname[] = "ADIOI_PMEM_WriteContig";
+#endif
+#ifdef DEBUG
+  int myrank, nprocs;
+#endif
+  MPI_Count datatype_size, len;
 
-    *error_code = MPI_SUCCESS;
+  *error_code = MPI_SUCCESS;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    MPI_Type_size_x(datatype, &datatype_size);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_WriteContig called on %s\n", myrank,
-            nprocs, fd->filename);
-    FPRINTF(stdout, "[%d/%d]    writing (buf = %p, loc = %lld, sz = %lld)\n",
-            myrank, nprocs, buf, (long long) offset, (long long) datatype_size * (long long) count);
+  MPI_Type_size_x(datatype, &datatype_size);
+  len = datatype_size * (ADIO_Offset)count;
+  if (file_ptr_type == ADIO_INDIVIDUAL) {
+    offset = fd->fp_ind;
+  }
 
-    if (file_ptr_type != ADIO_EXPLICIT_OFFSET) {
-        fd->fp_ind += datatype_size * count;
-        fd->fp_sys_posn = fd->fp_ind;
-        FPRINTF(stdout, "[%d/%d]    new file position is %lld\n", myrank,
-                nprocs, (long long) fd->fp_ind);
-    } else {
-        fd->fp_sys_posn = offset + datatype_size * count;
-    }
+#ifdef DEBUG
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_WriteContig called on %s\n", myrank,
+          nprocs, fd->filename);
+  FPRINTF(stdout, "[%d/%d]    writing (buf = %p, loc = %lld, sz = %lld)\n",
+          myrank, nprocs, buf, (long long)offset, (long long)len);
+#endif
+
+  ADIOI_PMEM_fs *pmemfs = fd->fs_ptr;
+  if (pmembb_bb_pwrite(pmemfs->bb, buf, len, offset) < 0) {
+    *error_code = ADIOI_Err_create_code(myname, fd->filename, errno);
+    fd->fp_sys_posn = -1;
+    return;
+  }
+
+  fd->fp_sys_posn = offset + len;
+  if (file_ptr_type == ADIO_INDIVIDUAL) {
+    fd->fp_ind += len;
+  }
 
 #ifdef HAVE_STATUS_SET_BYTES
-    MPIR_Status_set_bytes(status, datatype, datatype_size * count);
+  MPIR_Status_set_bytes(status, datatype, len);
 #endif
 }
 
 void ADIOI_PMEM_WriteStrided(ADIO_File fd, const void *buf, int count,
-                               MPI_Datatype datatype, int file_ptr_type,
-                               ADIO_Offset offset, ADIO_Status * status, int *error_code)
-{
-    int myrank, nprocs;
+                             MPI_Datatype datatype, int file_ptr_type,
+                             ADIO_Offset offset, ADIO_Status *status,
+                             int *error_code) {
+  *error_code = MPI_SUCCESS;
+#ifdef DEBUG
+  int myrank, nprocs;
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_WriteStrided called on %s\n", myrank,
+          nprocs, fd->filename);
+  FPRINTF(stdout, "[%d/%d]    calling ADIOI_GEN_WriteStrided\n", myrank,
+          nprocs);
+#endif
 
-    *error_code = MPI_SUCCESS;
-
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_PMEM_WriteStrided called on %s\n",
-            myrank, nprocs, fd->filename);
-    FPRINTF(stdout, "[%d/%d]    calling ADIOI_GEN_WriteStrided\n", myrank, nprocs);
-
-    ADIOI_GEN_WriteStrided(fd, buf, count, datatype, file_ptr_type, offset, status, error_code);
+  ADIOI_GEN_WriteStrided(fd, buf, count, datatype, file_ptr_type, offset,
+                         status, error_code);
 }
