9092504eca5cbf740c96c9f5b0058bc40b979567
range3 <white.line.oar@gmail.com>
2022-05-23 09:28:18 +0000

    Impl ADIOI_CHFS_Flush|Resize

--- src/mpi/romio/adio/ad_chfs/ad_chfs_flush.c
+++ src/mpi/romio/adio/ad_chfs/ad_chfs_flush.c
@@ -6,13 +6,18 @@
 #include "ad_chfs.h"
 #include "adioi.h"
 
-void ADIOI_CHFS_Flush(ADIO_File fd, int *error_code)
-{
-    int myrank, nprocs;
+void ADIOI_CHFS_Flush(ADIO_File fd, int* error_code) {
+  static char myname[] = "ADIOI_CHFS_Flush";
+#ifdef DEBUG
+  int myrank, nprocs;
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_CHFS_Flush called on %s\n", myrank, nprocs,
+          fd->filename);
+#endif
 
-    *error_code = MPI_SUCCESS;
-
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_CHFS_Flush called on %s\n", myrank, nprocs, fd->filename);
+  *error_code = MPI_SUCCESS;
+  if (chfs_fsync(fd->fd_sys)) {
+    *error_code = ADIOI_Err_create_code(myname, fd->filename, errno);
+  }
 }
--- src/mpi/romio/adio/ad_chfs/ad_chfs_resize.c
+++ src/mpi/romio/adio/ad_chfs/ad_chfs_resize.c
@@ -6,13 +6,19 @@
 #include "ad_chfs.h"
 #include "adioi.h"
 
-void ADIOI_CHFS_Resize(ADIO_File fd, ADIO_Offset size, int *error_code)
-{
-    int myrank, nprocs;
+void ADIOI_CHFS_Resize(ADIO_File fd, ADIO_Offset size, int* error_code) {
+  static char myname[] = "ADIOI_CHFS_Resize";
+#ifdef DEBUG
+  int myrank, nprocs;
+  MPI_Comm_size(fd->comm, &nprocs);
+  MPI_Comm_rank(fd->comm, &myrank);
+  FPRINTF(stdout, "[%d/%d] ADIOI_CHFS_Resize called on %s, sz=%d\n", myrank,
+          nprocs, fd->filename, size);
+#endif
 
-    *error_code = MPI_SUCCESS;
+  *error_code = MPI_SUCCESS;
 
-    MPI_Comm_size(fd->comm, &nprocs);
-    MPI_Comm_rank(fd->comm, &myrank);
-    FPRINTF(stdout, "[%d/%d] ADIOI_CHFS_Resize called on %s\n", myrank, nprocs, fd->filename);
+  if (chfs_truncate(fd->filename, size)) {
+    *error_code = ADIOI_Err_create_code(myname, fd->filename, errno);
+  }
 }
