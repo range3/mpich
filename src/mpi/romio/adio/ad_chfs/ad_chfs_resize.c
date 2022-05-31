169e2c248ea3fc22f5a67ad0b43c52359caa71ec
range3 <white.line.oar@gmail.com>
2022-05-31 10:46:39 +0000

    Update ADIOI_CHFS_Resize
    chfs_truncate is now only called by rank 0.
    using MPI_Barrier, MPI_Bcast to ensure process synchronization.

--- src/mpi/romio/adio/ad_chfs/ad_chfs_resize.c
+++ src/mpi/romio/adio/ad_chfs/ad_chfs_resize.c
@@ -8,17 +8,26 @@
 
 void ADIOI_CHFS_Resize(ADIO_File fd, ADIO_Offset size, int* error_code) {
   static char myname[] = "ADIOI_CHFS_Resize";
-#ifdef DEBUG
-  int myrank, nprocs;
-  MPI_Comm_size(fd->comm, &nprocs);
+  int ret;
+  int nprocs, myrank;
+
   MPI_Comm_rank(fd->comm, &myrank);
+#ifdef DEBUG
+  MPI_Comm_size(fd->comm, &nprocs);
   FPRINTF(stdout, "[%d/%d] ADIOI_CHFS_Resize called on %s, sz=%d\n", myrank,
           nprocs, fd->filename, size);
 #endif
 
   *error_code = MPI_SUCCESS;
+  MPI_Barrier(fd->comm);
 
-  if (chfs_truncate(fd->filename, size)) {
+  if (myrank == 0) {
+    ret = chfs_truncate(fd->filename, size);
+  }
+
+  MPI_Bcast(&ret, 1, MPI_INT, 0, fd->comm);
+
+  if (ret != 0) {
     *error_code = ADIOI_Err_create_code(myname, fd->filename, errno);
   }
 }
